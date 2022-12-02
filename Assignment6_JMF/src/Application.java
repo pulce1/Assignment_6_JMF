8963df768e295cd8aff16f24f14ebbf513c6d437
james <james@DESKTOP-KIL47CM.student.bellarmine.edu>
2022-12-01 19:24:50 -0500

    first commit

---
+++ Assignment6_JMF/src/Application.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * @author Nathan Johnson, Bellarmine University
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args Tree Height
+	 */
+	public static void main(String[] args)
+	{
+		//System.out.println("Creating a tree of height "+ args[0]+".");
+		Tree aTree=new Tree();//Creates a tree with root
+		Node root=aTree.generatePracticeTree(5);
+		DFS aSearch=new DFS();
+		int branch=0;
+		aSearch.DFS(root, branch);
+		
+		int height = aSearch.height;
+		int treeHeight = height+1;
+		
+		System.out.println("Tree Height: " + treeHeight);
+	}
+
+}
---
+++ Assignment6_JMF/src/DFS.java
@@ -0,0 +1,63 @@
+/**
+ * This Class is the class used in the application to use recursion to traverse a tree, printing each step
+ * @author James Frayser
+ * @version 12/1/2022
+ *
+ */
+public class DFS 
+{
+
+	/**
+	 * Methods for the root, travel distance, and height of the tree
+	 */
+	Node root;
+	int travel=0;
+	int height=0;
+	
+	public void DepthFirstSearch() 
+	{
+		
+		System.out.println("Call DFS with the root node to do a Depth First Search.");
+	}//end DepthFirstSearch method
+	
+	/**
+	 * 
+	 * @param node tells which node is about to be traversed
+	 * @param level tells what level of nodes you are on
+	 */
+	public void DFS(Node node, int level) 
+	{
+		
+		System.out.println(node.getData());
+	
+		/**
+		 * as node is not null, height becomes the level and the travel increases, marking distance in app
+		 */
+		if(node != null)
+		{
+			height=level;
+			travel++;
+			System.out.println("Nodes Traveled: " + travel);
+		}
+		
+		/**
+		 * @return the left node, As long as it is there and increases the level
+		 */
+		if(node.getlChild()!=null) 
+		{
+			DFS(node.getlChild() ,level+1);
+		}
+		
+		/**
+		 * @return the left node, As long as it is there and increases the level
+		 */
+		if(node.getrChild()!=null) 
+		{
+			DFS(node.getrChild(), level+1);
+		}
+		
+	
+		
+	}
+
+}
